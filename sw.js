39574f5a1e86f70ffd577dd27bf5ceaffc11de6f
bramhesh007 <120256886+bramhesh007@users.noreply.github.com>
2024-11-23 01:35:46 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+self.addEventListener('push', function(e) {
+    var options = {
+      body: 'This notification was generated from a push!',
+      icon: 'images/example.png',
+      sound:'notification.mp3',
+      vibrate: [100, 50, 100],
+      data: {
+        dateOfArrival: Date.now(),
+        primaryKey: '2'
+      },
+      actions: [
+        {action: 'explore', title: 'Explore this new world',
+          icon: 'images/checkmark.png'},
+        {action: 'close', title: 'Close',
+          icon: 'images/xmark.png'},
+      ]
+    };
+    e.waitUntil(
+      self.registration.showNotification('test message', options)
+    );
+  });
